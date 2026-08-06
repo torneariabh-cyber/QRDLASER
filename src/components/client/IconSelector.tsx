1a861e4eb763fdc7b6193f12ec4d7b8cbafe505c
bhrecuperadora <torneariabh@gmail.com>
2026-08-05 22:36:07 -0400

    Add IconSelector component for icon selection

--- src/components/client/IconSelector.tsx
+++ src/components/client/IconSelector.tsx
@@ -1,1 +1,36 @@
+import React from 'react'
+import { ICONS } from '../../utils/icons'
 
+interface IconSelectorProps {
+  selectedIcon: string
+  onSelectIcon: (icon: string) => void
+}
+
+const IconSelector: React.FC<IconSelectorProps> = ({ selectedIcon, onSelectIcon }) => {
+  return (
+    <div className="icon-grid">
+      <button
+        type="button"
+        className={`icon-option ${selectedIcon === '' ? 'selected' : ''}`}
+        onClick={() => onSelectIcon('')}
+      >
+        <span>✖</span>
+        <span className="icon-label">Nenhum</span>
+      </button>
+      
+      {ICONS.map((icon) => (
+        <button
+          key={icon.symbol}
+          type="button"
+          className={`icon-option ${selectedIcon === icon.symbol ? 'selected' : ''}`}
+          onClick={() => onSelectIcon(icon.symbol)}
+        >
+          <span className="icon-symbol">{icon.symbol}</span>
+          <span className="icon-label">{icon.name}</span>
+        </button>
+      ))}
+    </div>
+  )
+}
+
+export default IconSelector
