9922bd630b6ec655f79bdab44f59fa68d0e81ac1
bhrecuperadora <torneariabh@gmail.com>
2026-08-05 22:36:33 -0400

    Add LivePreview component with props and rendering

--- src/components/client/LivePreview.tsx
+++ src/components/client/LivePreview.tsx
@@ -1,1 +1,27 @@
+import React from 'react'
 
+interface LivePreviewProps {
+  name: string
+  font: string
+  icon: string
+}
+
+const LivePreview: React.FC<LivePreviewProps> = ({ name, font, icon }) => {
+  const displayText = icon ? `${icon} ${name}` : name
+
+  return (
+    <div className="live-preview">
+      <h4>👀 Prévia</h4>
+      <div className="preview-box">
+        <span
+          id="previewText"
+          style={{ fontFamily: `'${font}', sans-serif` }}
+        >
+          {displayText}
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default LivePreview
